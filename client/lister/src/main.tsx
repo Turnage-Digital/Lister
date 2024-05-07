c31638a9107c066e9458e9244b409a18e82e5c36
Heath Turnage <ibanezht@icloud.com>
2024-05-06 21:34:22 -0400

    I want to like react buddy but it fails me.

--- client/lister/src/main.tsx
+++ client/lister/src/main.tsx
@@ -8,12 +8,10 @@ import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { DevSupport } from "@react-buddy/ide-toolbox";
 
 import router from "./router";
 import theme from "./theme";
 import { AuthProvider, SideDrawerProvider } from "./components";
-import { ComponentPreviews, useInitial } from "./dev";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement!);
@@ -26,12 +24,7 @@ root.render(
         <LocalizationProvider dateAdapter={AdapterDateFns}>
           <AuthProvider>
             <SideDrawerProvider>
-              <DevSupport
-                ComponentPreviews={ComponentPreviews}
-                useInitialHook={useInitial}
-              >
-                <RouterProvider router={router} />
-              </DevSupport>
+              <RouterProvider router={router} />
             </SideDrawerProvider>
           </AuthProvider>
         </LocalizationProvider>
