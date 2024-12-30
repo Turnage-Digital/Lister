9faf208b1f060e8c9db9d493122358d78ed15c75
Heath Turnage <ibanezht@icloud.com>
2024-12-30 16:00:02 -0500

    Working in a Project level layout so that each page doesn't have to repeat itself.

--- src/Modules/Lists/Lister.Lists.Presentation/Pages/Index.cshtml.cs
+++ src/Modules/Lists/Lister.Lists.Presentation/Pages/Index.cshtml.cs
@@ -3,11 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Lister.Lists.Presentation.Pages;
 
-[Authorize]
 public class IndexModel : PageModel
 {
-    public void OnGet()
-    {
-        
-    }
+    public void OnGet() { }
 }
