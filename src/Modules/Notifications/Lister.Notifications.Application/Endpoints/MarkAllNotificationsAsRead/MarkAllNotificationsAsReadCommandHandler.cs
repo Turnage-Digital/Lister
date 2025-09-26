3526cc500fd8cda854085cedf4e3343a2338729e
Heath Turnage <ibanezht@icloud.com>
2025-09-26 15:48:48 -0400

    Cleanup.

--- src/Modules/Notifications/Lister.Notifications.Application/Endpoints/MarkAllNotificationsAsRead/MarkAllNotificationsAsReadCommandHandler.cs
+++ src/Modules/Notifications/Lister.Notifications.Application/Endpoints/MarkAllNotificationsAsRead/MarkAllNotificationsAsReadCommandHandler.cs
@@ -5,20 +5,15 @@ using MediatR;
 namespace Lister.Notifications.Application.Endpoints.MarkAllNotificationsAsRead;
 
 public class
-    MarkAllNotificationsAsReadCommandHandler<TRule, TNotification> : IRequestHandler<MarkAllNotificationsAsReadCommand>
+    MarkAllNotificationsAsReadCommandHandler<TRule, TNotification>(
+        NotificationAggregate<TRule, TNotification> aggregate
+    ) : IRequestHandler<MarkAllNotificationsAsReadCommand>
     where TRule : IWritableNotificationRule
     where TNotification : IWritableNotification
 {
-    private readonly NotificationAggregate<TRule, TNotification> _aggregate;
-
-    public MarkAllNotificationsAsReadCommandHandler(NotificationAggregate<TRule, TNotification> aggregate)
-    {
-        _aggregate = aggregate;
-    }
-
     public async Task Handle(MarkAllNotificationsAsReadCommand request, CancellationToken cancellationToken)
     {
-        await _aggregate.MarkAllNotificationsAsReadAsync(
+        await aggregate.MarkAllNotificationsAsReadAsync(
             request.UserId,
             DateTime.UtcNow,
             request.Before,
